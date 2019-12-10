4b1ae1c4ececcacb85a6c426f5bef6227e286730
Riza Kholifah Akwan <riza.kholifah05@gmail.com>
2019-12-10 10:54:58 +0700

    Menambahkan dan edit file belajarIF.java

---
+++ belajarIF.java
@@ -0,0 +1,10 @@
+public class belajarIF{
+    public static void main(String[] args){
+        boolean hujan = true;
+        if(hujan == false){
+            System.out.println("Bawa Payung");
+            System.exit(1);
+        }
+            System.out.println("Tidak usah Bawa Payung");
+    }
+}
