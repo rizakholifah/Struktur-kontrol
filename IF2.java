3b849247efc2a5785f7a01f0fc84520d8a23a681
Riza Kholifah Akwan <riza.kholifah05@gmail.com>
2019-12-10 11:07:51 +0700

    Menambahkan, edit dan kompilasi file IF2.java

---
+++ IF2.java
@@ -0,0 +1,11 @@
+public class IF2{
+    public static void main(String[] args){
+        int nilai = Integer.parseInt(args[0]);
+        if(nilai > 75){
+            System.out.println("Anda Lulus");
+        }
+        else{
+            System.out.println("Anda tidak lulus");
+        }
+    }
+}
